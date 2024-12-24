277f0122948bd750e18017fd872ae07254a9bb23
Ryan Baker <rbaker@xtier.com>
2024-12-24 15:53:37 -0500

    enabled stdout_buffer

--- lua/neotex/commands.lua
+++ lua/neotex/commands.lua
@@ -7,11 +7,9 @@ M.compile = function()
     vim.fn.mkdir(config.build_dir, 'p')
 
     local cmd = { config.latex_cmd, "-output-directory=" .. config.build_dir, file }
-    print(config.build_dir)
-    print(config.latex_cmd)
 
     vim.fn.jobstart(cmd, {
-        stdout_buffered = false,
+        stdout_buffered = true,
         on_exit = function(_, code)
             if code == 0 then
                 print("Compilation successful.")
