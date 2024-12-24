227d8c35031d34f5abec7ad9c7b9d889f5bcaab3
Ryan Baker <rbaker@xtier.com>
2024-12-24 16:34:07 -0500

    Replaced io.write with print

--- lua/colorprint.lua
+++ lua/colorprint.lua
@@ -1,43 +1,35 @@
 local M = {}
 
-function M.black(text, eol)
-    eol = eol or '\n'
-    io.write("\27[30m" .. text .. "\27[0m" .. eol)
+function M.black(text)
+    print("\27[30m" .. text .. "\27[0m")
 end
 
-function M.red(text, eol)
-    eol = eol or '\n'
-    io.write("\27[31m" .. text .. "\27[0m" .. eol)
+function M.red(text)
+    print("\27[31m" .. text .. "\27[0m")
 end
 
-function M.green(text, eol)
-    eol = eol or '\n'
-    io.write("\27[32m" .. text .. "\27[0m" .. eol)
+function M.green(text)
+    print("\27[32m" .. text .. "\27[0m")
 end
 
-function M.yellow(text, eol)
-    eol = eol or '\n'
-    io.write("\27[33m" .. text .. "\27[0m" .. eol)
+function M.yellow(text)
+    print("\27[33m" .. text .. "\27[0m")
 end
 
-function M.blue(text, eol)
-    eol = eol or '\n'
-    io.write("\27[34m" .. text .. "\27[0m" .. eol)
+function M.blue(text)
+    print("\27[34m" .. text .. "\27[0m")
 end
 
-function M.purple(text, eol)
-    eol = eol or '\n'
-    io.write("\27[35m" .. text .. "\27[0m" .. eol)
+function M.purple(text)
+    print("\27[35m" .. text .. "\27[0m")
 end
 
-function M.cyan(text, eol)
-    eol = eol or '\n'
-    io.write("\27[36m" .. text .. "\27[0m" .. eol)
+function M.cyan(text)
+    print("\27[36m" .. text .. "\27[0m")
 end
 
-function M.gray(text, eol)
-    eol = eol or '\n'
-    io.write("\27[37m" .. text .. "\27[0m" .. eol)
+function M.gray(text)
+    print("\27[37m" .. text .. "\27[0m")
 end
 
 return M
