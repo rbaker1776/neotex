ad7afea266d9be59e7e5ec18aa950c6c58bf9050
Ryan Baker <rbaker@xtier.com>
2024-12-24 14:13:10 -0500

    noremap = true

--- lua/neotex/keymaps.lua
+++ lua/neotex/keymaps.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 M.setup = function()
-    vim.keymap.set('n', '<leader>lc', ":lua require('neotex.commands').compile()<CR>")
+    vim.keymap.set(
+    'n',
+    '<leader>lc',
+    ":lua require('neotex.commands').compile()<CR>",
+    { noremap = true, silent = true })
 end
 
 return M
