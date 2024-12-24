1da765c229652af14bfe76b3e044d8cc5c49a11e
Ryan Baker <rbaker@xtier.com>
2024-12-24 16:03:09 -0500

    Changing default behavior to output a .pdf to the same dir

--- lua/neotex/commands.lua
+++ lua/neotex/commands.lua
@@ -4,17 +4,38 @@ local M = {}
 
 M.compile = function()
     local file = vim.fn.expand("%:p")
-    vim.fn.mkdir(config.build_dir, 'p')
 
-    local cmd = { config.latex_cmd, "-output-directory=" .. config.build_dir, file }
+    -- ensure we are working with a LaTeX file
+    if not file:match("%.tex$") then
+        print("(neotex) Error: Current file is not a LaTeX file.")
+        return
+    end
+
+    -- validate the LaTeX command
+    if not vim.fn.executable(config.latex_cmd) then
+        print("(neotex) Error: LaTeX command '" .. config.latex_cmd .. "' is not executable.")
+        return
+    end
+
+    local cmd = { config.latex_cmd, file }
 
     vim.fn.jobstart(cmd, {
         stdout_buffered = true,
+        on_stdout = function(_, data)
+            if data then
+                print("(neotex) STDOUT: " .. table.concat(data, '\n'))
+            end
+        end,
+        on_stderr = function(_, data)
+            if data then
+                print("(neotex) STDERR: " .. table.concat(data, '\n'))
+            end
+        end,
         on_exit = function(_, code)
             if code == 0 then
-                print("Compilation successful.")
+                print("(neotex) Compilation successful.")
             else
-                print("Compilation failed.")
+                pritn("(neotex) Error: Compilation failed.")
             end
         end,
     })
