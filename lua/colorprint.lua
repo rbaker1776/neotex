9ebe8827ffb1f21122979a61fdc9e07a568dceb2
Ryan Baker <rbaker@xtier.com>
2024-12-24 16:39:31 -0500

    switched to \x1b

--- lua/colorprint.lua
+++ lua/colorprint.lua
@@ -2,42 +2,42 @@ local M = {}
 
 function M.black(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[30m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[30m" .. text .. "\x1b[0m" .. eol)
 end
 
 function M.red(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[31m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[31m" .. text .. "\x1b[0m" .. eol)
 end
 
 function M.green(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[32m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[32m" .. text .. "\x1b[0m" .. eol)
 end
 
 function M.yellow(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[33m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[33m" .. text .. "\x1b[0m" .. eol)
 end
 
 function M.blue(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[34m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[34m" .. text .. "\x1b[0m" .. eol)
 end
 
 function M.purple(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[35m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[35m" .. text .. "\x1b[0m" .. eol)
 end
 
 function M.cyan(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[36m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[36m" .. text .. "\x1b[0m" .. eol)
 end
 
 function M.gray(text, eol)
     eol = eol or '\n'
-    vim.api.nvim_out_write("\27[37m" .. text .. "\27[0m" .. eol)
+    vim.api.nvim_out_write("\x1b[37m" .. text .. "\x1b[0m" .. eol)
 end
 
 return M
