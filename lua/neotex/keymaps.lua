a374ae849b24ca8b509bf202c5540a99e22def1e
Ryan Baker <rbaker@xtier.com>
2024-12-24 14:05:43 -0500

    updated compile keymapping

--- lua/neotex/keymaps.lua
+++ lua/neotex/keymaps.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.setup = function()
-    vim.api.nvim_set_keymap('n', '<leader>lc', ":lua require('neotex.commands').compile()<CR>")
+    vim.keymap.set('n', '<leader>lc', ":lua require('neotex.commands').compile()<CR>")
 end
 
 return M
