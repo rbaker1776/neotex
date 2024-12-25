e0857b9c0830abf76d998d0ee6fe423c7ec21584
Ryan Baker <rbaker@xtier.com>
2024-12-25 14:10:54 -0500

    Used callbacks to return early from preview()

--- lua/neotex/commands.lua
+++ lua/neotex/commands.lua
@@ -3,77 +3,96 @@ local utils = require("neotex.utils")
 
 local M = {}
 
-M.compile = function()
+M.compile = function(on_complete)
     local file = vim.fn.expand("%:p")
     local pdf_file = vim.fn.expand("%:t:r") .. ".pdf"
     
     -- ensure we are working with a LaTeX file
     if not file:match("%.tex$") then
-        print("(neotex) Error: Current file is not a LaTeX file.")
+        vim.api.nvim_notify(
+            "(neotex) Error: Current file is not a LaTeX file.",
+        vim.log.levels.ERROR, {})
         return
     end
 
     -- validate the LaTeX command
     if not vim.fn.executable(config.latex_cmd) then
-        print("(neotex) Error: LaTeX command '" .. config.latex_cmd .. "' is not executable.")
+        vim.api.nvim_notify(
+            "(neotex) Error: LaTeX command '" .. config.latex_cmd .. "' is not executable.",
+        vim.log.levels.ERROR, {})
         return
     end
 
     local cmd = { config.latex_cmd, "-interaction=nonstopmode", "-synctex=1", file }
-
-    local success = false
 
     vim.fn.jobstart(cmd, {
         stdout_buffered = true,
         stderr_buffered = true,
         on_stdout = function(_, data)
             if data then
-                print("(neotex) STDOUT: " .. table.concat(data, '\n'))
+                vim.api.notify(
+                    "(neotex) STDOUT: " .. table.concat(data, '\n'),
+                vim.log.levels.INFO, {})
             end
         end,
         on_stderr = function(_, data)
             if data then
-                print("(neotex) STDERR: " .. table.concat(data, '\n'))
+                vim.api.notify(
+                    "(neotex) STDERR: " .. table.concat(data, '\n'),
+                vim.log.levels.ERROR, {})
             end
         end,
         on_exit = function(_, code)
             if code == 0 then
-                print("(neotex) Compilation successful.")
-                success = true
+                vim.api.notify(
+                    "(neotex) LaTeX compilation successful.",
+                vim.log.levels.INFO, {}) 
+                if on_complete then
+                    on_complete(true)
+                end
             else
-                print("(neotex) Error: Compilation failed.")
+                vim.api.notify(
+                    "(neotex) Error: LaTeX compilation failed.",
+                vim.log.levels.ERROR, {}) 
+                if on_complete then
+                    on_complete(false)
+                end
             end
         end,
     })
-
-    return success
 end
 
-M.open_pdf = function()
+M.open_pdf = function(on_complete)
     local pdf_file = vim.fn.expand("%:p:r") .. ".pdf"
 
     -- validate PDF viewer
     if not vim.fn.executable(config.pdf_viewer) then
-        print("(neotex) Error: PDF viewer '" .. "' is not found.")
+        vim.api.nvim_notify(
+            "(neotex) Error: PDF viewer '" .. "' is not found.",
+        vim.log.levels.ERROR, {})
         return
     end
 
     -- ensure target PDF exists
     if utils.file_exists(pdf_file) then
-        print("(neotex) Opening " .. pdf_file .. '.')
+        vim.api.notify(
+            "(neotex) Opening " .. pdf_file .. "...",
+        vim.log.levels.INFO, {})
         vim.fn.jobstart({ config.pdf_viewer, pdf_file }, { detach = true })
     else
-        print("(neotex) Error: File " .. pdf_file .. " does not exist.")
+        vim.api.notify(
+            "(neotex) Error: File " .. pdf_file .. " does not exist.",
+        vim.log.levels.ERROR, {})
     end
 end
 
 M.preview = function()
-    local did_compile = M.compile()
-    print(did_compile)
-    if not did_compile then
-        return
-    end
-    M.open_pdf()
+    M.compile(function(did_compile)
+        if not did_compile then
+            return
+        end
+        M.open_pdf()
+    end)
 end
 
 return M
--- lua/neotex/keymaps.lua
+++ lua/neotex/keymaps.lua
@@ -7,7 +7,13 @@ local keymap = vim.keymap
 M.setup = function()
 
     keymap.set('n', '<leader>lc', function()
-        commands.compile()
+        commands.compile(function(success)
+            if success then
+                print("(neotex) Compilation successful.")
+            else
+                print("(neotex) Error: Compilation failed.")
+            end
+        end)
     end, { noremap = true, silent = true, desc = "Compile LaTeX" })
 
     keymap.set('n', '<leader>lo', function()
