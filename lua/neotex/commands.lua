ff1a38c607b389449803bdf5e5b5b936f5bbee20
Ryan Baker <rbaker@xtier.com>
2024-12-24 20:28:35 -0500

    Added Zathura integration

--- lua/neotex/commands.lua
+++ lua/neotex/commands.lua
@@ -4,7 +4,8 @@ local M = {}
 
 M.compile = function()
     local file = vim.fn.expand("%:p")
-
+    local pdf_file = vim.fn.expand("%:t:r") .. ".pdf"
+    
     -- ensure we are working with a LaTeX file
     if not file:match("%.tex$") then
         print("(neotex) Error: Current file is not a LaTeX file.")
@@ -17,10 +18,11 @@ M.compile = function()
         return
     end
 
-    local cmd = { config.latex_cmd, "-interaction=nonstopmode", file }
+    local cmd = { config.latex_cmd, "-interaction=nonstopmode", "-synctex=1", file }
 
     vim.fn.jobstart(cmd, {
         stdout_buffered = true,
+        stderr_buffered = true,
         on_stdout = function(_, data)
             if data then
                 print("(neotex) STDOUT: " .. table.concat(data, '\n'))
@@ -33,9 +35,20 @@ M.compile = function()
         end,
         on_exit = function(_, code)
             if code == 0 then
-                print("(neotex) Compilation successful.")
+                print("(neotex) Compilation successful. Opening PDF...")
+                M.open_pdf(pdf_file)
             else
                 print("(neotex) Error: Compilation failed.")
+            end
+        end,
+    })
+end
+
+M.open_pdf = function(pdf_file)
+    vim.fn.jobstart({ "zathura", pdf_file }, {
+        on_exit = function(_, code)
+            if code ~= 0 then
+                print("(neotex) Error: Failed to open PDF with Zathura.")
             end
         end,
     })
