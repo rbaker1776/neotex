9b3353a3883b7dee0eeeb45424e0c696d1929ee9
Ryan Baker <rbaker@xtier.com>
2024-12-28 12:26:11 -0500

    Attempt to make double cursor expansion

--- lua/neotex/snippets/tex.lua
+++ lua/neotex/snippets/tex.lua
@@ -2,11 +2,12 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
 
 return {
     s("env", {
-        t("\\begin{"), i(1, "environment"), t("}"),
-        t({"", '\t'}), i(0),
-        t({"", "\\end{"}), i(1, "environment"), t("}"),
+        t("\\begin{"), i(1, "environment"), t("}"), t({"", '\t'}), i(0),
+        t({"", "\\end{"}), f(function(args) return args[1][1] end, {1}),
+        t("}"),
     }),
 }
