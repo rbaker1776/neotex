a58128977965d7032ec3a9fea5061004a1e7a582
Ryan Baker <rbaker@xtier.com>
2024-12-28 12:31:27 -0500

    Attempt to make double cursor expansion

--- lua/neotex/snippets/tex.lua
+++ lua/neotex/snippets/tex.lua
@@ -3,7 +3,7 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
-
+--[[
 return {
     s("env", {
         t("\\begin{"), i(1, "environment"), t("}"), t({"", '\t'}), i(0),
@@ -20,4 +20,4 @@ return {
         f(function(args) return args[1][1] end, {1}), -- Dynamically mirrors the first input node
         t("}"),
     }),
-}
+}--]]
